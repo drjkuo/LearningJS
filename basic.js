3473f136861f2432a911d191a535a4ad2a2731e3
KUO <drjkuo@gmail.com>
2017-02-08 15:35:14 -0800

    object vs primitive value

--- basic.js
+++ basic.js
@@ -4,3 +4,14 @@
 
  console.log(obj.foo); //get
 
+
+// object
+var obj1 = {};
+var obj2 = {};
+console.log(ojb1 === obj2);
+console.log(ojb1 === obj1);
+
+//primitive values
+var prim1 = 111;
+var prim2 = 111;
+console.log(prim1 === prim2);
