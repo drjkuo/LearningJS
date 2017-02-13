69462410da5fc46b443786c57693035d3c61690a
KUO <drjkuo@gmail.com>
2017-02-12 21:45:26 -0800

    Function hoist

--- basic.js
+++ basic.js
@@ -15,3 +15,23 @@ console.log(obj1 === obj1);
 var prim1 = 111;
 var prim2 = 111;
 console.log(prim1 === prim2);
+
+
+foo();
+
+// function foo() {
+//     'use strict';
+//     bar();  // hoist
+//     function bar() {
+//       console.log("1");
+//     }
+// }
+
+function foo() {
+    'use strict';
+    bar();  // no hoist
+    var bar = function () {
+      console.log("111");
+    }
+}
+
